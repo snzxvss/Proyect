0b2b01ea2da5f9703e4388d95b16eb23b67a01bd
andres <andresjesusrobles@gmail.com>
2024-03-06 22:36:16 -0500

    first commit

---
+++ yuli/script.js
@@ -0,0 +1,12 @@
+$('.hero-btn').click(function(){
+  if(!$('.hero-btn').parent().hasClass('active')){
+    $(this).parent().stop().addClass('active');
+    setTimeout(function(){  
+      $('.hero-btn').parent().removeClass('active'); 
+    }, 2000);
+  }
+});
+function mostrarTextoAmor() {
+  var textoAmorElement = document.getElementById("textoAmor");
+  textoAmorElement.classList.remove("ocultar");
+}
